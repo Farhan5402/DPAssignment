036841c007ba98c797035c806a9c0de4b953750d
Aweb Mekhilef <awebmekhilef@gmail.com>
2022-01-05 10:33:40 +1100

    Added some comments

--- src/Main/Vehicle.java
+++ src/Main/Vehicle.java
@@ -5,19 +5,21 @@ import AbstractFactory.TrainCar;
 import Strategy.*;
 
 public abstract class Vehicle {
-	
-	// TODO: Maybe add setters for each of the below that set the values and images
+
 	/*
+	 * Maybe add setters for each of the below that set the values and images
+	 * 
+	 * // Assume chassis.place() returns the path to image file
+	 * public void setChassis(Chassis chassis) {
+	 * image = chassis.place();
+	 * }
+	 * 
+	 */
 
-	// Assume chassis.place() returns the path to image file
-	public void setChassis(Chassis chassis) {
-		image = chassis.place();
-	}
-
-	*/
-
-	// To call the strategy algorithm just do for example lightBehaviour.lightUp()
-	// There is no need for a commond to do that
+	/*
+	 * To call the strategy algorithm just do for example lightBehaviour.lightUp()
+	 * There is no need for a commond to do that
+	 */
 
 	public LightBehaviour lightBehaviour;
 	public MoveBehaviour moveBehaviour;
